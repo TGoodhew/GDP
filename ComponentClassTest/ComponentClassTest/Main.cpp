517820064e7c9b6a9455acd3b6cc301b7d7184c5
Tony Goodhew <tony_goodhew@hotmail.com>
2015-01-22 21:17:02 -0800

    Fixing createfile issue

--- ComponentClassTest/ComponentClassTest/Main.cpp
+++ ComponentClassTest/ComponentClassTest/Main.cpp
@@ -6,7 +6,7 @@
 #include "GarageDoorSenorArray.h"
 #include "UltrasonicSensor.h"
 
-CUltrasonicSensor mb1040(3, 1);
+CUltrasonicSensor* mb1040;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
@@ -15,14 +15,15 @@ int _tmain(int argc, _TCHAR* argv[])
 
 void setup()
 {
-
+	pinMode(6, OUTPUT);
+	mb1040 = new CUltrasonicSensor(3, 1);
 }
 
 // the loop routine runs over and over again forever:
 void loop()
 {
-	mb1040.TurnSensorOn();
+	mb1040->TurnSensorOn();
 	delay(1000);
-	mb1040.TurnSensorOff();
+	mb1040->TurnSensorOff();
 	delay(1000);
 }
